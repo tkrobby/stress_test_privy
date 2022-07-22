125b4a16f985aba98745f4690782735627b0aa98
Robby Tino Kurniawan <robby.tino@sicepat.com>
2022-07-23 01:27:23 +0700

    add pt loginn

--- login/login.js
+++ login/login.js
@@ -2,19 +2,23 @@ import http from 'k6/http';
 import { FormData } from 'https://jslib.k6.io/formdata/0.0.2/index.js';
 import { group, check } from 'k6';
 import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
+import papaparse from 'https://jslib.k6.io/papaparse/5.1.1/index.js';
+import { SharedArray } from 'k6/data';
+
+
 
 let base_url = 'http://pretest-qa.dcidev.id'
-let endpoint =  base_url + '/api/v1/register';
+let endpoint =  base_url + '/api/v1/oauth/sign_in';
 
 export function handleSummary(data) {
     return {
-      'register/summary.html': htmlReport(data),
+      'login/summary.html': htmlReport(data),
     };
 };
 
 export const options = {
     stages: [
-      { duration: "5s", target: 10 },
+      { duration: "5s", target: 5 },
     //   { duration: "30s", target: 10 },
     //   { duration: "1m", target: 10 },
     //   { duration: "30s", target: 5 },
@@ -22,30 +26,29 @@ export const options = {
     ],
 };
 
+
+const csvRead = new SharedArray("transaction date", function () {
+    return papaparse.parse(open('./data/data.csv'),{header: true}).data;
+})
+
+var param_phone = csvRead[Math.floor(Math.random() * csvRead.length)]['phone'];
+
+const bodyReq = {
+    phone: param_phone,
+    password: "123123",
+    latlong: "12312",
+    device_token: "desc",
+    device_type: "0"
+}
+
 export default function () {
-    //init data
-    let range = {min: 200, max: 1500}
-    let delta = range.max - range.min
-
-    const randomNumber = Math.round(range.min + Math.random() * delta)
-
-    let PhoneNumber = '628109100' + randomNumber
-    group('Register User', function () {
-        
-        const bodyReq = {
-            "phone": `${PhoneNumber}`,
-            "password": "123123",
-            "country": "ID",
-            "latlong": "‑6.200000-106.816666",
-            "device_token": "123",
-            "device_type": "0"
-        }
+    group('login', function () {
         let headers = {
             'content-type': 'application/x-www-form-urlencoded',
+            'Accept' : 'application/json'
         }
 
         let response = http.post(endpoint, bodyReq, {headers});
-        console.log(response.body);
         check(response, {
             'Response status is 201': (r) => r.status === 201,
             'status is not 400': (r) => r.status !== 400,
